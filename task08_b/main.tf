c1c3ec0120a9602baa953eb93c5292a0b45e459f
Sardor <tashkhodjaev.sardor@gmail.com>
2026-07-11 18:24:51 +0500

    6

--- task08_b/main.tf
+++ task08_b/main.tf
@@ -2,6 +2,23 @@ provider "azurerm" {
   features {}
 }
 
+provider "kubernetes" {
+  host                   = module.aks.host
+  client_certificate     = base64decode(module.aks.client_certificate)
+  client_key             = base64decode(module.aks.client_key)
+  cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+}
+
+provider "kubectl" {
+  host                   = module.aks.host
+  client_certificate     = base64decode(module.aks.client_certificate)
+  client_key             = base64decode(module.aks.client_key)
+  cluster_ca_certificate = base64decode(module.aks.cluster_ca_certificate)
+  load_config_file       = false
+
+}
+
+
 data "azurerm_client_config" "current" {}
 
 resource "azurerm_resource_group" "rg" {
